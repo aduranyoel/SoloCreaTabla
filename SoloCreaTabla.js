54ad7107c3eb818be74fe792f696c95d5b866a4a
Tony Duran <yoet92@gmail.com>
2019-08-01 14:47:01 -0300

    Completado

--- SoloCreaTabla.js
+++ SoloCreaTabla.js
@@ -1,26 +1,29 @@
 'use strict';
 function SoloCreaTabla(idTabla, options)
 {
-    var options             = options || {};
-    options.col             = options.col || [];
-    options.rows            = options.rows || [];
-    var tableElement        = document.getElementById(idTabla);
-    var theadElement        = document.createElement("thead");
-    options.col.forEach(function(e){
-        var thElement       = document.createElement("th");
-        thElement.innerHTML=e;
-        theadElement.appendChild(thElement);
-    })
-    tableElement.appendChild(theadElement);
-    var tbodyElement        = document.createElement("tbody");
-    options.rows.forEach(function(e){
-        var trElement       = document.createElement("tr");
-        e.forEach(function(x){
-            var tdElement   = document.createElement("td");
-            tdElement.innerHTML=x;
-            trElement.appendChild(tdElement);
-        })
-        tbodyElement.appendChild(trElement);
-    })
-    tableElement.appendChild(tbodyElement);
+    options = options || {};
+        options.col = options.col || [];
+        options.rows = options.rows || [];
+        var tableElement = document.getElementById(idTabla);
+        var theadElement = document.createElement("thead");
+        var trHeaderElement = document.createElement("tr");
+
+        options.col.forEach(function (e) {
+            var thElement = document.createElement("th");
+            thElement.innerHTML = e;
+            trHeaderElement.appendChild(thElement);
+        });
+        theadElement.appendChild(trHeaderElement);
+        tableElement.appendChild(theadElement);
+        var tbodyElement = document.createElement("tbody");
+        options.rows.forEach(function (e) {
+            var trElement = document.createElement("tr");
+            e.forEach(function (x) {
+                var tdElement = document.createElement("td");
+                tdElement.innerHTML = x;
+                trElement.appendChild(tdElement);
+            });
+            tbodyElement.appendChild(trElement);
+        });
+        tableElement.appendChild(tbodyElement);
 };
